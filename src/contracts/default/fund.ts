c1d6ae28295777758998415a13e3dcca774f1336
Adam Walker <adamwalker101@gmail.com>
2018-02-13 18:31:32 +0000

    Add & Transfer Demo contract
    contract demonstrating adding funds and transferring funds between accounts.

--- src/contracts/default/fund.ts
+++ src/contracts/default/fund.ts
@@ -28,6 +28,15 @@ export default class Fund extends Standard {
   private outputActivity: Activity;
 
   /**
+   * Cache the input activity
+   *
+   * @private
+   * @type {Activity}
+   * @memberof Fund
+   */
+  private inputActivity: Activity;
+
+  /**
    * Quick Check, Allow all data but make sure it is signatureless
    *
    * @param {boolean} signatureless
@@ -52,35 +61,13 @@ export default class Fund extends Standard {
    */
   public vote(): Promise<boolean> {
     return new Promise<boolean>((resolve, reject) => {
-      // Lets see what we have
-      ActiveLogger.debug(this.transactions, "TX");
-
-      // Get the input to verify (and prepare)
-      let inputStreams = Object.keys(this.transactions.$i);
-
-      // Only manage 1 input for now
-      let inputActivity = this.getActivityStreams(inputStreams[0]);
-
-      // Check they can issue
-
-      // Get the output
-      let outputStreams = Object.keys(this.transactions.$o);
-
-      // Only manage 1 output for now
-      this.outputActivity = this.getActivityStreams(outputStreams[0]);
-
-      // Output State
-      let state = this.outputActivity.getState();
-
-      // Now prepare
-      this.prepare = this.transactions.$i[inputStreams[0]];
-
-      // Have we already got this symbol?
-      if (state.funds && state.funds[this.prepare.symbol]) {
-        return reject("Fund Symbol Exists");
-      } else {
-        // Approve
-        resolve(true);
+      switch (this.transactions.$entry) {
+        case "transfer":
+          this.voteTransfer(resolve, reject);
+          break;
+        default:
+          this.voteAdd(resolve, reject);
+          break;
       }
     });
   }
@@ -93,18 +80,165 @@ export default class Fund extends Standard {
    */
   public commit(): Promise<any> {
     return new Promise<any>((resolve, reject) => {
-      // Output State
-      let state = this.outputActivity.getState();
-
-      // Make sure funds exists
-      if (!state.funds) state.funds = {};
-
-      // Update Funds
-      state.funds[this.prepare.symbol] = this.prepare.amount;
-
-      // Update State
-      this.outputActivity.setState(state);
-      resolve(true);
+      switch (this.transactions.$entry) {
+        case "transfer":
+          this.commitTransfer(resolve, reject);
+          break;
+        default:
+          this.commitAdd(resolve, reject);
+          break;
+      }
     });
+  }
+
+  /**
+   * Add Funds to the account voting phase
+   *
+   * @private
+   * @param {((value?: boolean | PromiseLike<boolean> | undefined) => void)} resolve
+   * @param {(reason?: any) => void} reject
+   * @returns {void}
+   * @memberof Fund
+   */
+  private voteAdd(
+    resolve: (value?: boolean | PromiseLike<boolean> | undefined) => void,
+    reject: (reason?: any) => void
+  ): void {
+    // Lets see what we have
+    ActiveLogger.debug(this.transactions, "TX");
+
+    // Get the input to verify (and prepare)
+    let inputStreams = Object.keys(this.transactions.$i);
+
+    // Only manage 1 input for now
+    let inputActivity = this.getActivityStreams(inputStreams[0]);
+
+    // Check they can issue
+
+    // Get the output
+    let outputStreams = Object.keys(this.transactions.$o);
+
+    // Only manage 1 output for now
+    this.outputActivity = this.getActivityStreams(outputStreams[0]);
+
+    // Output State
+    let state = this.outputActivity.getState();
+
+    // Now prepare
+    this.prepare = this.transactions.$i[inputStreams[0]];
+
+    // Have we already got this symbol?
+    if (state.funds && state.funds[this.prepare.symbol]) {
+      return reject("Fund Symbol Exists");
+    } else {
+      // Approve
+      resolve(true);
+    }
+  }
+
+  /**
+   * Add Funds to the account commit phase
+   *
+   * @private
+   * @param {((value?: boolean | PromiseLike<boolean> | undefined) => void)} resolve
+   * @param {(reason?: any) => void} reject
+   * @memberof Fund
+   */
+  private commitAdd(
+    resolve: (value?: boolean | PromiseLike<boolean> | undefined) => void,
+    reject: (reason?: any) => void
+  ): void {
+    // Output State
+    let state = this.outputActivity.getState();
+
+    // Make sure funds exists
+    if (!state.funds) state.funds = {};
+
+    // Update Funds
+    state.funds[this.prepare.symbol] = this.prepare.amount;
+
+    // Update State
+    this.outputActivity.setState(state);
+    resolve(true);
+  }
+
+  /**
+   * Move Funds from one account to another voting phase
+   *
+   * @private
+   * @param {((value?: boolean | PromiseLike<boolean> | undefined) => void)} resolve
+   * @param {(reason?: any) => void} reject
+   * @returns {void}
+   * @memberof Fund
+   */
+  private voteTransfer(
+    resolve: (value?: boolean | PromiseLike<boolean> | undefined) => void,
+    reject: (reason?: any) => void
+  ): void {
+    // Lets see what we have
+    ActiveLogger.debug(this.transactions, "TX");
+
+    // Get the input to verify (and prepare)
+    let inputStreams = Object.keys(this.transactions.$i);
+
+    // Only manage 1 input for now
+    this.inputActivity = this.getActivityStreams(inputStreams[0]);
+
+    // Get the output
+    let outputStreams = Object.keys(this.transactions.$o);
+
+    // Only manage 1 output for now
+    this.outputActivity = this.getActivityStreams(outputStreams[0]);
+
+    // Input State
+    let state = this.inputActivity.getState();
+
+    // Now prepare
+    this.prepare = this.transactions.$i[inputStreams[0]];
+
+    // Have we already got this symbol and enough to transfer
+    if (
+      state.funds &&
+      state.funds[this.prepare.symbol] &&
+      state.funds[this.prepare.symbol] < this.prepare.amount
+    ) {
+      return reject("Fund Symbol doesn't exist or not enough balance");
+    } else {
+      // Approve
+      resolve(true);
+    }
+  }
+
+  /**
+   * Move Funds from one account to another commit phase
+   *
+   * @private
+   * @param {((value?: boolean | PromiseLike<boolean> | undefined) => void)} resolve
+   * @param {(reason?: any) => void} reject
+   * @memberof Fund
+   */
+  private commitTransfer(
+    resolve: (value?: boolean | PromiseLike<boolean> | undefined) => void,
+    reject: (reason?: any) => void
+  ): void {
+    // Output State
+    let state = this.outputActivity.getState();
+
+    // Make sure funds exists
+    if (!state.funds) state.funds = {};
+
+    // Update Funds
+    state.funds[this.prepare.symbol] = this.prepare.amount;
+
+    // Update State
+    this.outputActivity.setState(state);
+
+    // Now deduct from the input
+    state = this.inputActivity.getState();
+    state.funds[this.prepare.symbol] -= this.prepare.amount;
+    this.inputActivity.setState(state);
+
+    // Return
+    resolve(true);
   }
 }
