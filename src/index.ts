0e973f1729d2f8d4b4e3dcc4da48d0ac2c4146c5
Adam Walker <adamwalker101@gmail.com>
2018-04-25 15:14:43 +0100

    Ready to release

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,35 @@
+#!/usr/bin/env node
+
+/*
+ * MIT License (MIT)
+ * Copyright (c) 2018 Activeledger
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 import * as cluster from "cluster";
 import * as os from "os";
 import * as fs from "fs";
 import * as minimist from "minimist";
-import { ActiveNetwork, ActiveInterfaces } from "activenetwork";
-import { ActiveLogger } from "activelogger";
-import { ActiveCrypto } from "activecrypto";
+import { ActiveNetwork, ActiveInterfaces } from "@activeledger/activenetwork";
+import { ActiveLogger } from "@activeledger/activelogger";
+import { ActiveCrypto } from "@activeledger/activecrypto";
 
 // Process Arguments
 // TOOD: Change solution to static class
