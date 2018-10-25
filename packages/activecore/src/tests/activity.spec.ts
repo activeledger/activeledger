314ef8434c34d30366dff632fb10f4e6505ce1e5
Adam Walker <adamwalker101@gmail.com>
2018-10-25 10:48:47 +0100

    Old V3 Assets

--- packages/activecore/src/tests/activity.spec.ts
+++
@@ -1,9 +0,0 @@
-var should    = require('chai').should();
-var supertest = require('supertest');
-var api       = supertest('http://localhost:3000/api');
-
-describe('Activity unit tests:', () => {
-    it('Should create a Activity instance', (done: Function) => {
-        api.post('/Activity').send({}).expect(200, done);
-    });
-});
