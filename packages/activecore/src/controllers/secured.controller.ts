7fdc8b679a239fcace9f32c09368479a6c760cf1
Adam Walker <adamwalker101@gmail.com>
2018-12-05 22:17:12 +0000

    Improved Core E/D

--- packages/activecore/src/controllers/secured.controller.ts
+++ packages/activecore/src/controllers/secured.controller.ts
@@ -34,7 +34,10 @@ export class SecuredController {
    *
    * @memberof SecuredController
    */
-  constructor() {}
+  constructor() {
+    // Setup Mock Network Home
+    ActiveledgerDatasource.getSelfNeighbour();
+  }
 
   /**
    * Expose helpful encrypt method (Add warning about not SSL)
@@ -44,22 +47,38 @@ export class SecuredController {
    * @memberof SecuredController
    */
   @post("/api/secured/encrypt", {
-    description: "Warning Core doesn't natively support HTTPS so any data you send is clear text. Localhost or testing only.",
+    description:
+      "Warning Core doesn't natively support HTTPS so any data you send is clear text. Localhost or testing usage only recommended.",
     responses: {
       "200": {
-        description: "Activity Stream Volatile Data",
+        description: "Nested Permissioned Encrypted Data",
         content: {
           "application/json": {
             schema: {
               type: "object",
-              properties: {},
-              additionalProperties: true
+              properties: {
+                $ADAC: {
+                  type: "array",
+                  items: {
+                    type: "object",
+                    properties: {
+                      ref: { type: "string" },
+                      type: { type: "string" },
+                      roles: { type: "string" }
+                    }
+                  }
+                },
+                data: {
+                  type: "object",
+                  additionalProperties: true
+                }
+              }
             }
           }
         }
       },
-      "404": {
-        description: "Activity Stream Not Found",
+      "500": {
+        description: "Failed to Encrypt",
         content: {
           "application/json": {
             schema: {
@@ -75,18 +94,58 @@ export class SecuredController {
       }
     }
   })
-  async encrypt(@requestBody() data: any): Promise<any> {
-    let home = ActiveledgerDatasource.getSelfNeighbour();
-    let secured = new ActiveCrypto.Secured(
-      ActiveledgerDatasource.getDb(),
-      ActiveledgerDatasource.getNeighbourhood(),
-      {
-        reference: ActiveNetwork.Home.reference,
-        public: Buffer.from(ActiveNetwork.Home.publicPem,"base64").toString("utf8"),
-        private: ActiveNetwork.Home.identity.pem
+  encrypt(
+    @requestBody({
+      required: true,
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              $ADAC: {
+                type: "array",
+                items: {
+                  type: "object",
+                  properties: {
+                    ref: { type: "string" },
+                    type: { type: "string" },
+                    roles: { type: "array", items: { type: "string" } }
+                  }
+                }
+              },
+              data: {
+                type: "object",
+                additionalProperties: true
+              }
+            }
+          }
+        }
       }
-    );
-    return await secured.encrypt(data);
+    })
+    data: any
+  ): Promise<{}> {
+    return new Promise((resolve, reject) => {
+      let secured = new ActiveCrypto.Secured(
+        ActiveledgerDatasource.getDb(),
+        ActiveledgerDatasource.getNeighbourhood(),
+        {
+          reference: ActiveNetwork.Home.reference,
+          public: Buffer.from(ActiveNetwork.Home.publicPem, "base64").toString(
+            "utf8"
+          ),
+          private: ActiveNetwork.Home.identity.pem
+        }
+      );
+      secured
+        .encrypt(data)
+        .then(resolve)
+        .catch(error => {
+          resolve({
+            statusCode: 500,
+            message: error
+          });
+        });
+    });
   }
 
   /**
@@ -97,24 +156,22 @@ export class SecuredController {
    * @memberof SecuredController
    */
   @post("/api/secured/decrypt", {
-    description: "Warning Core doesn't natively support HTTPS so any data you send is clear text. Localhost or testing only.",
+    description:
+      "Warning Core doesn't natively support HTTPS so any data you receive is clear text. Localhost or testing usage only recommended.",
     responses: {
       "200": {
-        description: "Activity Stream Volatile Data",
+        description: "Nested Permissioned Decrypted Data",
         content: {
           "application/json": {
             schema: {
               type: "object",
-              properties: {
-                success: { type: "boolean" }
-              },
               additionalProperties: true
             }
           }
         }
       },
-      "404": {
-        description: "Activity Stream Not Found",
+      "500": {
+        description: "Failed to Decrypt",
         content: {
           "application/json": {
             schema: {
@@ -130,17 +187,59 @@ export class SecuredController {
       }
     }
   })
-  async decrypt(@requestBody() data: any): Promise<any> {
-    let home = ActiveledgerDatasource.getSelfNeighbour();
-    let secured = new ActiveCrypto.Secured(
-      ActiveledgerDatasource.getDb(),
-      ActiveledgerDatasource.getNeighbourhood(),
-      {
-        reference: ActiveNetwork.Home.reference,
-        public: Buffer.from(ActiveNetwork.Home.publicPem,"base64").toString("utf8"),
-        private: ActiveNetwork.Home.identity.pem
+  async decrypt(
+    @requestBody({
+      required: true,
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              $ADAC: {
+                type: "array",
+                items: {
+                  type: "object",
+                  properties: {
+                    ref: { type: "string" },
+                    type: { type: "string" },
+                    roles: { type: "string" }
+                  }
+                }
+              },
+              data: {
+                type: "object",
+                additionalProperties: true
+              }
+            }
+          }
+        }
       }
-    );
-    return await secured.decrypt(data);
+    })
+    data: any
+  ): Promise<any> {
+    return new Promise((resolve, reject) => {      
+      let secured = new ActiveCrypto.Secured(
+        ActiveledgerDatasource.getDb(),
+        ActiveledgerDatasource.getNeighbourhood(),
+        {
+          reference: ActiveNetwork.Home.reference,
+          public: Buffer.from(ActiveNetwork.Home.publicPem, "base64").toString(
+            "utf8"
+          ),
+          private: ActiveNetwork.Home.identity.pem
+        }
+      );
+      secured
+        .decrypt(data)
+        .then((result: any) => {
+          resolve(result.data);
+        })
+        .catch(error => {
+          resolve({
+            statusCode: 500,
+            message: error
+          });
+        });
+    });
   }
 }
