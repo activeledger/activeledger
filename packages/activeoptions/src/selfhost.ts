bbe256b25671d8df0f4814935c0eaf8c98d052ac
Adam Walker <adamwalker101@gmail.com>
2019-01-17 17:09:44 +0000

    Starting mapping pouch http endpoints

--- packages/activeoptions/src/selfhost.ts
+++ packages/activeoptions/src/selfhost.ts
@@ -23,7 +23,7 @@
 import * as http from "http";
 import * as path from "path";
 import * as fs from "fs";
-import { ActiveHttpd } from "./httpd";
+import { ActiveHttpd, IActiveHttpIncoming } from "./httpd";
 import PouchDB from "./pouchdb";
 
 (function() {
@@ -38,7 +38,7 @@ import PouchDB from "./pouchdb";
 
   function getPDB(name: string) {
     if (!pDBCache[name]) {
-      pDBCache[name] = new PouchDb("pouch__all_dbs__");
+      pDBCache[name] = new PouchDb(name);
     }
     return pDBCache[name];
   }
@@ -47,7 +47,7 @@ import PouchDB from "./pouchdb";
   let http = new ActiveHttpd();
 
   // Index
-  http.use("/", () => {
+  http.use("/", "ALL", () => {
     return {
       activeledger: "Welcome to Activeledger data!",
       adapters: ["leveldb"]
@@ -55,57 +55,140 @@ import PouchDB from "./pouchdb";
   });
 
   // Standard Session
-  http.use("_session", () => {
+  http.use("_session", "ALL", () => {
     return { ok: true, userCtx: { name: null, roles: ["_admin"] } };
   });
 
   // List all databases
-  http.use("_all_dbs", async () => {
+  http.use("_all_dbs", "ALL", async () => {
     // Get Database
     let db = getPDB("pouch__all_dbs__");
 
     // Search for other databases
-    let docs = await db.allDocs({
-      startkey: "db_",
-      endkey: "db_" + "\uffff"
-    });
+    let docs = await db.allDocs();
 
     // Correct output
     return docs.rows.map((e: any) => e.id.replace("db_", ""));
   });
 
   // Get Nested stuff
-  http.use("*/*/hello", () => {
-    return "it works";
+  http.use("*/*/hello", "ALL", (incoming: IActiveHttpIncoming) => {
+    return `it works - path = ${incoming.url[0]}/${incoming.url[1]}/${
+      incoming.url[2]
+    } `;
+  });
+
+  // Get Database Info
+  http.use("*", "GET", async (incoming: IActiveHttpIncoming) => {
+    // Get Database
+    let db = getPDB(incoming.url[0]);
+    return await db.info();
+  });
+
+  // Create Database
+  http.use(
+    "*",
+    "PUT",
+    async (incoming: IActiveHttpIncoming, req: http.IncomingMessage) => {
+      // Get Database
+      let db = getPDB(incoming.body.name);
+
+      // Create Database
+      await db.info();
+
+      // Add to Database Cache
+      db = getPDB("pouch__all_dbs__");
+
+      // Add to cache
+      await db.put({
+        _id: "db_" + incoming.body.name
+      });
+
+      return { ok: true };
+    }
+  );
+
+  // Create Database
+  http.use(
+    "*",
+    "DELETE",
+    async (incoming: IActiveHttpIncoming, req: http.IncomingMessage) => {
+      let db = getPDB(incoming.url[0]);
+      // Delete Database
+      await db.destory(incoming.url[0]);
+
+      // Remove Cache db
+      db = getPDB("pouch__all_dbs__");
+
+      // Add to cache
+      await db.remove({
+        _id: "db_" + incoming.url[0]
+      });
+
+      return { ok: true };
+    }
+  );
+
+  // Get all docs from a database
+  http.use("*/_all_docs", "GET", async (incoming: IActiveHttpIncoming) => {
+    // Get Database
+    let db = getPDB(incoming.url[0]);
+
+    // Convert Limit
+    if (incoming.query.limit) {
+      incoming.query.limit = parseInt(incoming.query.limit) || null;
+    }
+
+    return await db.allDocs(incoming.query);
+  });
+
+  // Get specific docs from a database
+  http.use("*/*", "GET", async (incoming: IActiveHttpIncoming) => {
+    // Get Database
+    let db = getPDB(incoming.url[0]);
+    try {
+      return await db.get(incoming.url[1]);
+    } catch (e) {
+      console.log(e);
+      return "";
+    }
   });
 
   // Fauxton
-  http.use("_utils", (req: http.IncomingMessage, res: http.ServerResponse) => {
-    // We want to force /_utils to /_utils/ as this is the CouchDB behavior
-    if (req.url === "/_utils") {
-      res.writeHead(301, {
-        Location: "/_utils/"
-      });
-      return;
-    }
+  http.use(
+    "_utils/**",
+    "ALL",
+    (
+      incoming: IActiveHttpIncoming,
+      req: http.IncomingMessage,
+      res: http.ServerResponse
+    ) => {
+      // We want to force /_utils to /_utils/ as this is the CouchDB behavior
+      if (req.url === "/_utils") {
+        res.writeHead(301, {
+          Location: "/_utils/"
+        });
+        return;
+      }
 
-    // File to send
-    let file = FAUXTON_PATH + "/index.html";
+      // File to send
+      let file = FAUXTON_PATH + "/index.html";
 
-    // If path is not default overwrite
-    if (req.url !== "/_utils/") {
-      file = FAUXTON_PATH + (req.url as string).replace("/_utils", "");
-    }
+      // If path is not default overwrite
+      if (req.url !== "/_utils/") {
+        file = FAUXTON_PATH + (req.url as string).replace("/_utils", "");
+      }
 
-    if (fs.existsSync(file)) {
-      res.setHeader(
-        "Content-type",
-        ActiveHttpd.mimeType[path.parse(file).ext] || "text/plain"
-      );
-      // Convert To Stream
-      return fs.readFileSync(file);
+      if (fs.existsSync(file)) {
+        res.setHeader(
+          "Content-type",
+          ActiveHttpd.mimeType[path.parse(file).ext] || "text/plain"
+        );
+        // Convert To Stream
+        return fs.readFileSync(file);
+      }
     }
-  });
+  );
 
   // Start Server
   http.listen(parseInt(process.argv[3]));
