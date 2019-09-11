1179d2fb290909bc5edcf0d809df7f0a9d59c396
Adam <adamwalker101@gmail.com>
2019-09-11 11:09:09 +0100

    Update the tests so that don't fail due to big 2.1 changes

--- tests/vm.test.ts
+++ tests/vm.test.ts
@@ -6,60 +6,63 @@ import "mocha";
 
 describe("Virtual Machine Test (Activeprotocol)", () => {
   // Some Random Test Data
-  const random = Math.floor(Math.random() * 100000000 + 1).toString();
+  const random = Math.floor(Math.random() * 100000000 + 1);
 
   let VM: VirtualMachine;
 
   it("should create secured VM", () => {
     VM = new VirtualMachine(
-      "",
       "localhost:5259",
-      "",
-      new Date(),
-      {
-        $namespace: "default",
-        $contract: "onboard",
-        $i: {
-          testStreamId: {
-            publicKey:
-              "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwUYrUdC11Y+0vH8JD7FC\nlVXTnStllicQ1PPgbrsftuxWJFqE1FPa6qwwdo6y0ZjQTmwsPUSTzLiX5Kr1UVdY\nFZVP/DDeWafOwh80vMZpBuUcxl4hh6IEXf8BtKelwUIq1gmwR2QS6LPjG9UeblBg\nq6a0h3Z1LAB/Ls9+78Rs9zuSwKYBdvn1IT6jwOkQ2eZj+8JO0vDr6VYUzenu+HAG\nuzq+ioHaS9uVn3Vlo7p5FjkXdI0g8rkC8kiwh+dW4d4Qdep0nRSjCUhpYpQhifjp\nHLM/Mtpkbc0gGgUu7NSuHkNBYUbl9TAOQkZNaU1rgYWt0gZ3jMQuttOCfF55We72\niwIDAQAB\n-----END PUBLIC KEY-----"
-          }
-        },
-        $o: {}
-      },
-      {
-        testStreamId:
-          "mJbmGI2VdTMC6u+/mQroEPe4unnIVtTr9W85NGsrEAi5jCG4Kt3X+yGMMYQ79wEzTyFpkcyIKHpXDfiRZLInP1BiI5ukdjI+rRNbelK0ueGLh9rHElkxm3znwxRt1ZQeXaFzUtmdLtvo1jhxxZeEa3Hs9AmvTCKWx96KJKaSaREokY3UARZZZzTKIHR5rTL4nrjd8v2g+/hWMzcm1XugjfDNqBRST8J0LYXlyvFnLOE/fc40P8JI3FKDXy/aWHS5sH9CoJDP3kYho/07Tfo4/H0jZGrJ0YQ15kc6ZUonxYRlGNpO+kpsWjhksAKVmgNMsho1nEf7d39i0Dn3aKG/Tg==",
-        $sig: ""
-      },
-      [],
-      [],
-      [],
+      new ActiveCrypto.Secured({} as IActiveDSConnect, [], {}) as any, // Fix private type
       {} as any,
-      {} as any,
-      new ActiveCrypto.Secured({} as IActiveDSConnect, [], {}) as any // Fix private type
+      {} as any
     );
     expect(VM).to.be.an("object");
   });
 
   it("should initalise VM contract as promised", () => {
-    expect(VM.initalise()).to.be.a("promise");
+    expect(
+      VM.initialise(
+        {
+          contractLocation: "./test/",
+          umid: "test",
+          date: new Date(),
+          remoteAddress: "localhost",
+          transaction: {
+            $namespace: "test",
+            $contract: "test",
+            $i: {},
+            $o: {}
+          },
+          signatures: {
+            testStreamId:
+              "mJbmGI2VdTMC6u+/mQroEPe4unnIVtTr9W85NGsrEAi5jCG4Kt3X+yGMMYQ79wEzTyFpkcyIKHpXDfiRZLInP1BiI5ukdjI+rRNbelK0ueGLh9rHElkxm3znwxRt1ZQeXaFzUtmdLtvo1jhxxZeEa3Hs9AmvTCKWx96KJKaSaREokY3UARZZZzTKIHR5rTL4nrjd8v2g+/hWMzcm1XugjfDNqBRST8J0LYXlyvFnLOE/fc40P8JI3FKDXy/aWHS5sH9CoJDP3kYho/07Tfo4/H0jZGrJ0YQ15kc6ZUonxYRlGNpO+kpsWjhksAKVmgNMsho1nEf7d39i0Dn3aKG/Tg==",
+            $sig: ""
+          },
+          inputs: [],
+          outputs: [],
+          readonly: {},
+          key: random
+        },
+        "test"
+      )
+    ).to.be.a("promise");
   });
 
   it("should run smart contract verify as promised", () => {
-    expect(VM.verify(true)).to.be.a("promise");
+    expect(VM.verify(true, "test")).to.be.a("promise");
   });
 
   it("should run smart contract vote as promised", () => {
-    expect(VM.vote()).to.be.a("promise");
+    expect(VM.vote("test")).to.be.a("promise");
   });
 
   it("should run smart contract commit as promised", () => {
-    expect(VM.commit({} as any, false)).to.be.a("promise");
+    expect(VM.commit({} as any, false, "test")).to.be.a("promise");
   });
 
   it("should get transaction inputs from VM", () => {
-    expect(VM.getInputs())
+    expect(VM.getInputs("test"))
       .to.be.an("array")
       .length(0);
   });
