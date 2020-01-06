acdaba61b56a5360e8bc33cddc6ee01c854514ab
Adam Walker <adamwalker101@gmail.com>
2020-01-06 17:57:36 +0000

    CLI Started, Mainnet Pushes to CLI. + Planning

---
+++ packages/hybrid/src/index.ts
@@ -0,0 +1,125 @@
+#!/usr/bin/env node
+
+/*
+ * MIT License (MIT)
+ * Copyright (c) 2018 Activeledger
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+import * as fs from "fs";
+import { ActiveOptions } from "@activeledger/activeoptions";
+import { ActiveLogger } from "@activeledger/activelogger";
+import { ActiveHttpd, IActiveHttpIncoming } from "@activeledger/httpd";
+
+// Initalise CLI Options
+ActiveOptions.init();
+
+//#region Check & Manage Configuration File
+if (!fs.existsSync(ActiveOptions.get<string>("config", "./config.json"))) {
+    // Read default config so we can add our identity to the neighbourhood
+    let defConfig: any = JSON.parse(
+        fs.readFileSync(__dirname + "/default.config.json", "utf8")
+    );
+
+    // Adjusting Ports (Check for default port)
+    if (
+        ActiveOptions.get<boolean>("port", false) &&
+        ActiveOptions.get<number>("port", 5260) !== 5260
+    ) {
+        // Update Node Host
+        defConfig.host =
+            ActiveOptions.get<string>("host", "127.0.0.1") +
+            ":" +
+            ActiveOptions.get<string>("port", 5260);
+
+        // Update Self host
+        defConfig.db.selfhost.port = (
+            parseInt(ActiveOptions.get<string>("port", 5260)) - 1
+        ).toString();
+    }
+
+    // Data directory passed?
+    if (ActiveOptions.get<boolean>("data-dir", false)) {
+        defConfig.db.selfhost.dir = ActiveOptions.get<string>("data-dir", "");
+    }
+
+    // lets write the default one in this location
+    fs.writeFileSync(
+        ActiveOptions.get<string>("config", "./config.json"),
+        JSON.stringify(defConfig)
+    );
+    ActiveLogger.info(
+        "Created Hybrid Config File"
+    );
+}
+//#endregion
+ActiveOptions.parseConfig();
+
+// Basic check for database and config
+if (ActiveOptions.get("db", false)) {
+
+    // Create Database
+
+    // Create Light Server
+    let http = new ActiveHttpd(true);
+
+    http.use("/", "POST", async (incoming: IActiveHttpIncoming, r: any) => {
+        console.log(incoming.body);
+        // Check Token And "From Server"
+
+        // Run Transaction
+
+        // Worked or failed we should still resend to nodes connected to us!
+
+        // NOT FOR NOW : With IoT and core no auth, I wonder if the solution
+        // is they sign a code with their key to get the SSE connection accepted?
+        // doing this means we could also send in real-time from a mainnet node
+        // However for now best to keep in here less overhead and less chance of vunrabilities
+
+        // If everything is ok, return ok
+
+        // If failed report as error to database and remote
+
+        // Remote may then send the "latest version"
+
+        // As we will be single threaded for the moment it shouldnt be an issue for fast transactions.
+        // However we may need to do some rev matching.
+
+        // Remote Side if ok then skip
+        // Remote side if not 200 status code store for later push
+
+        return { status: "ok" };
+
+    })
+
+
+    // Testing Purposes
+    http.use("/", "GET", async () => {
+        return { hello: "world" }
+    })
+
+    // Listen!
+    const [, port] = ActiveOptions.get<String>("host", ":5260").split(":");
+    http.listen(parseInt(port), true);
+    ActiveLogger.info("Activecore API is running at 0.0.0.0:" + port);
+
+} else {
+    ActiveLogger.fatal("Configuration file incomplete");
+    process.exit(0);
+}
